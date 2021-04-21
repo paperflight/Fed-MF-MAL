Fix the error in reward calculation: the sinr_clip should be cut from qos instead of sinr itself

memory.py
@@ -144,7 +144,7 @@ class ReplayMemory:
         action = torch.tensor([transition[self.history - 1].action], dtype=torch.int64, device=self.device)
         avail = torch.tensor([transition[self.history - 1].avail]).to(dtype=torch.bool).to(device=self.device)
         # Calculate truncated n-step discounted return R^n = Σ_k=0->n-1 (γ^k)R_t+k+1 (note that invalid nth next states have reward 0)
-        R = torch.stack([sum(self.discount ** n * (transition[self.history + n - 1].reward - transition[self.history + n - 1].avg)
+        R = torch.stack([sum(self.discount ** n * (transition[self.history + n - 1].reward - avg)
                              for n in range(self.n))]).to(device=self.device).to(dtype=torch.float32)
         # Mask for non-terminal nth next states
         nonterminal = torch.tensor([transition[self.history + self.n - 1].nonterminal], dtype=torch.float32,
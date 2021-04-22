Add the gradient crop Fix the error in non-episodic learning. The log probability should not be used to update the average reward.

agent.py
@@ -212,14 +212,17 @@ class Agent:
                                   (pns_a * (b - l.float())).view(-1))  # m_u = m_u + p(s_t+n, a*)(b - l)
 
         loss = -torch.sum(m * log_ps_a, 1)  # Cross-entropy loss (minimises DKL(m||p(s_t, a_t)))
-        self.average_reward = self.average_reward + \
-                              self.reward_update_rate * torch.mean(returns.unsqueeze(1) +
-                                               pns_a.detach() * self.support - log_ps_a.detach() * self.support)
-        self.average_reward = self.average_reward.detach()
         self.online_net.zero_grad()
         (weights * loss).mean().backward()  # Backpropagate importance-weighted minibatch loss
-        # clip_grad_norm_(self.online_net.parameters(), 1.0, norm_type=1)
+        clip_grad_norm_(self.online_net.parameters(), 10.0, norm_type=2)
         self.optimiser.step()
+
+        ps = self.online_net(states)  # Log probabilities log p(s_t, ·; θonline)
+        ps_a = log_ps[range(self.batch_size), actions]  # log p(s_t, a_t; θonline)
+        self.average_reward = self.average_reward + \
+                              self.reward_update_rate * torch.mean(returns.unsqueeze(1) +
+                                                                   pns_a.detach() * self.support - ps_a.detach() * self.support)
+        self.average_reward = self.average_reward.detach()
 
         mem.update_priorities(idxs, loss.detach().cpu().numpy())  # Update priorities of sampled transitions
 

env.py
@@ -417,7 +417,7 @@ class Channel:
                                                              axis=1))), axis=1), "SINR_DISTANCE", gp.UE_COLOR)
         return sinr, action, actual_action
 
-    def decentralized_reward_moving(self, sinr):
+    def decentralized_reward_moving(self, sinr, aa):
         sinr = np.log2(sinr + 1)
         self.user_qos[:, 0] -= sinr
         self.user_qos[:, 1] -= 1
@@ -445,7 +445,7 @@ class Channel:
         self.user_number = np.sum(rest)
         return ap_distribute_reward
 
-    def decentralized_reward(self, sinr):
+    def decentralized_reward(self, sinr, aa):
         sinr_clip = np.log2(sinr + 1)
         sinr_clip[sinr_clip > gp.USER_QOS] = gp.USER_QOS
         # sinr_clip /= gp.USER_QOS
@@ -531,6 +531,8 @@ class Channel:
         self.user_qos[:, 1] -= 1
         rest = np.all(self.user_qos > 0, axis=1)
         gain = np.logical_and(self.user_qos[:, 0] <= 0, self.user_qos[:, 1] >= 0)
+
+        sinr_clip[gain] += self.user_qos[gain, 0]
 
         # sinr_clip = (np.log10(sinr_clip / gp.USER_QOS + 1) * 2 - 0.35) * 5
         ap_observe_relation = np.stack([self.user_position] * self.ap_position.shape[0], axis=0) \

memory.py
@@ -153,8 +153,8 @@ class ReplayMemory:
         while not valid:
             samples = np.random.uniform(0.0, segment_length,
                                         [batch_size]) + segment_starts  # Uniformly sample from within all segments
-            probs, idxs, tree_idxs = self.transitions.find(
-                samples)  # Retrieve samples from tree with un-normalised probability
+            probs, idxs, tree_idxs = self.transitions.find(samples)
+            # Retrieve samples from tree with un-normalised probability
             if np.all((self.transitions.index - idxs) % self.capacity > self.n) and np.all(
                     (idxs - self.transitions.index) % self.capacity >= self.history) and np.all(probs != 0):
                 valid = True  # Note that conditions are valid but extra conservative around buffer index 0
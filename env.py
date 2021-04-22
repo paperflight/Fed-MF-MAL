Add the gradient crop Fix the error in non-episodic learning. The log probability should not be used to update the average reward.

env.py
@@ -491,13 +491,9 @@ class Channel:
         self.user_qos[:, 1] -= 1
         rest = np.all(self.user_qos > 0, axis=1)
         gain = np.logical_and(self.user_qos[:, 0] <= 0, self.user_qos[:, 1] >= 0)
-        # sinr_clip = (np.log10(sinr_clip / gp.USER_QOS + 1) * 2 - 0.35) * 5
 
-        # sinr_x = self.sinr_ap_user_noncoop()
-        # sinr_x[np.where(sinr_clip == 0)] = 0
-        # # sinr_x[sinr_x > 8] = 8
-        # sinr_x = np.log10(sinr_x / 8 + 1) * 2
-        # sinr_clip = sinr_clip - sinr_x
+        sinr_clip[gain] += self.user_qos[gain, 0]
+
         ap_observe_relation = np.stack([self.user_position] * self.ap_position.shape[0], axis=0) \
                               - np.stack([self.ap_position] * self.user_position.shape[0], axis=1)
         ap_observe_relation_edg = np.all(np.absolute(ap_observe_relation) < int(gp.REWARD_CAL_RANGE *
@@ -505,10 +501,11 @@ class Channel:
                                          axis=2)
         ap_observe_relation = np.all(np.absolute(ap_observe_relation) < int((gp.ACCESS_POINTS_FIELD - 1) / 2), axis=2)
         ap_observe_relation = np.logical_and(ap_observe_relation_edg, ap_observe_relation)
-        ap_distribute_reward = ap_observe_relation * gain
+        ap_distribute_reward = ap_observe_relation * sinr_clip
         normalized_factor = np.sum(ap_observe_relation, axis=1)
         normalized_factor[normalized_factor == 0] = 1
-        ap_distribute_reward = np.sum(ap_distribute_reward, axis=1) / normalized_factor
+        ap_distribute_reward = np.sum(ap_distribute_reward, axis=1) / \
+                               (gp.USER_WAITING / gp.USER_ADDING * gp.DENSE_OF_USERS / self.ap_number)
         # normalization
 
         if gp.USER_WAITING == 1:
@@ -519,7 +516,7 @@ class Channel:
             self.user_position = self.user_position[rest]
             self.user_qos = self.user_qos[rest]
             self.user_number = np.sum(rest)
-        # ap_distribute_reward[np.where(aa == 12)[0]] = -0.5
+        ap_distribute_reward[np.where(aa == 12)[0]] = -0.5
         # ap_distribute_reward[ap_distribute_reward > 2] = 2
         return ap_distribute_reward / 5
 
@@ -543,7 +540,8 @@ class Channel:
         ap_distribute_reward = ap_observe_relation_edg * np.absolute(ap_observe_relation_cet - 1) * sinr_clip
         normalized_factor = np.sum(np.logical_and(ap_observe_relation_edg, np.absolute(ap_observe_relation_cet - 1)), axis=1)
         normalized_factor[normalized_factor == 0] = 1
-        ap_distribute_reward = np.sum(ap_distribute_reward, axis=1) / normalized_factor
+        ap_distribute_reward = np.sum(ap_distribute_reward, axis=1) / \
+                               (gp.USER_WAITING / gp.USER_ADDING * gp.DENSE_OF_USERS / self.ap_number)
         # normalization
 
         if gp.USER_WAITING == 1:
@@ -603,7 +601,7 @@ class Channel:
             # self.small_scale_fading = self.small_scale_fading[:, rest]
             # self.coop_decision = self.coop_decision[:, rest]
             # self.channel = self.channel[:, rest]
-        ap_distribute_reward[np.where(action == 12)[0]] = 0
+        # ap_distribute_reward[np.where(action == 12)[0]] = 0
         # ap_distribute_reward[ap_distribute_reward > 2] = 2
         return ap_distribute_reward / 5
 
@@ -618,7 +616,7 @@ if __name__ == "__main__":
     res_avg = np.zeros(20)
     for _ in range(1000):
         sinr, action, aa = x.test_sinr('random')
-        res = x.decentralized_reward_directional(sinr, aa)
+        res = x.decentralized_reward_step(sinr, aa)
         # x.random_action('updown', x.coop_graph.calculate_action_mask())
         # res1 = x.decentralized_reward_exclude_central(x.sinr_calculation())
         res_avg += res
@@ -629,7 +627,7 @@ if __name__ == "__main__":
     res_avg1 = np.zeros(20)
     for _ in range(1000):
         sinr, action, aa = x.test_sinr('updown')
-        res = x.decentralized_reward_directional(sinr, aa)
+        res = x.decentralized_reward_step(sinr, aa)
         res_avg1 += res
     res_avg1 /= 1000
     print(res_avg1)